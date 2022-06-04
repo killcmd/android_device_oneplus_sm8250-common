badcea23ba20c646715f549750323663ae3bb69a
Ferdinand Vasquez <killcmd@gmail.com>
2022-06-04 10:25:44 -0400

    SQUEEZE: URL?

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Fetching the big files..."
 echo "GoogleCamera"
-wget -q -O "$ANDROID_BUILD_TOP/device/oneplus/sm8250-common/bottle/needed/GoogleCamera/GoogleCamera.apk" https://1-dontsharethislink.celsoazevedo.com/file/filesc/GCam8.2.204_Greatness.220428.0041Release_Snap.apk
+wget -q -O "$ANDROID_BUILD_TOP/device/oneplus/sm8250-common/bottle/needed/GoogleCamera/GoogleCamera.apk" https://gitlab.com/killcmd/googlecamera/-/raw/main/GCAM.apk?inline=false
