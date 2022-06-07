c0aabf64084ccfa2319037daf9725ec18606ca19
Ferdinand Vasquez <killcmd@gmail.com>
2022-06-06 22:58:55 -0400

    SQUEEZE: Comments for future changes.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Fetching the big files..."
-echo "GoogleCamera"
-wget -q -O "$ANDROID_BUILD_TOP/device/oneplus/sm8250-common/bottle/needed/GoogleCamera/GoogleCamera.apk" https://gitlab.com/killcmd/googlecamera/-/raw/main/GCAM.apk?inline=false
+## Paths
+# $ANDROID_BUILD_TOP/device/oneplus/sm8250-common/bottle/needed
+# $ANDROID_BUILD_TOP/device/oneplus/sm8250-common/bottle/custom
+
+wget -O "$PWD/device/oneplus/sm8250-common/bottle/needed/GoogleCamera/GoogleCamera.apk" https://gitlab.com/killcmd/googlecamera/-/raw/main/GCAM.apk?inline=false
