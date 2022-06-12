6dd9438595becee3d82f23ba269bab29299058b1
Ferdinand Vasquez <killcmd@gmail.com>
2022-06-12 18:24:20 -0400

    vendorsetup: Syntax Corrections.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
 echo "Fetching the big files..."
-## Paths
-# $ANDROID_BUILD_TOP/device/oneplus/sm8250-common/bottle/needed
-# $ANDROID_BUILD_TOP/device/oneplus/sm8250-common/bottle/custom
-
-wget -O "$PWD/device/oneplus/sm8250-common/bottle/needed/GoogleCamera/GoogleCamera.apk" https://gitlab.com/killcmd/googlecamera/-/raw/main/GCAM.apk?inline=false
+wget -O "$PWD/device/oneplus/sm8250-common/bottle/needed/GoogleCamera/GoogleCamera.apk" "https://gitlab.com/killcmd/googlecamera/-/raw/main/GCAM.apk?inline=false"
