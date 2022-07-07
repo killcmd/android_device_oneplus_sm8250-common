309c38108c7f2df625934426468d6ace48d6f733
Ferdinand Vasquez <killcmd@gmail.com>
2022-07-07 19:18:36 -0400

    Arcana match up

--- bottle/vars.mk
+++ bottle/vars.mk
@@ -1,15 +1,3 @@
-ifeq ($(SNACK),ARROW)
-ARROW_GAPPS := true
-$(call inherit-product, device/oneplus/sm8250-common/bottle/custom/custom.mk)
-# ARROW_COMMUNITY := true
-ARROW_BETA := true
-endif
-ifeq ($(SNACK),ARROWPLUS)
-ARROW_GAPPS := true
-$(call inherit-product, device/oneplus/sm8250-common/bottle/custom/gaming.mk)
-# ARROW_COMMUNITY := true
-ARROW_BETA := true
-endif
 ifeq ($(SNACK),SPARK)
 WITH_GAPPS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -19,7 +7,21 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
 TARGET_BUILD_LAWNCHAIR := false
 endif
-
+ifeq ($(SNACK),ARCANA)
+ARCANA_MAINTAINER := Ferdinand Vasquez (killcmd)
+WITH_GAPPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
+TARGET_BUILD_LAWNCHAIR := false
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_SUPPORTS_BLUR := true
+COPY_APN_SYSTEM := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+USE_PIXEL_CHARGER_IMAGES := true
+endif
 # General Included
 $(call inherit-product, device/oneplus/sm8250-common/bottle/custom/custom.mk)
 $(call inherit-product, device/oneplus/sm8250-common/bottle/needed/needed.mk)
