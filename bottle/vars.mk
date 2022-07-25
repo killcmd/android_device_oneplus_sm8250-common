30da5a9feccedd54f0cd40773b9390b33c9313f5
Ferdinand Vasquez <killcmd@gmail.com>
2022-07-24 20:38:57 -0400

    Cherish: Init 3.9

--- bottle/vars.mk
+++ bottle/vars.mk
@@ -22,6 +22,19 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_NOW_PLAYING := true
 USE_PIXEL_CHARGER_IMAGES := true
 endif
+ifeq ($(SNACK),CHERISH)
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_ADAPTIVE_CHARGING := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_SUPPORTS_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+# Inherit some props from Cherish
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=killcmd
+endif
 # General Included
 $(call inherit-product, device/oneplus/sm8250-common/bottle/custom/custom.mk)
 $(call inherit-product, device/oneplus/sm8250-common/bottle/needed/needed.mk)
+
